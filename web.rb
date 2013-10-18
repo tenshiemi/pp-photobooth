fde4464d7effa66bc5680b8b8b7489250a831c6f
RaphaelEidus <raphaeleidus@gmail.com>
2013-10-18 17:56:58 -0400

    to_s

--- web.rb
+++ web.rb
@@ -39,7 +39,7 @@ post '/save' do
   Base64.decode64(image_data)
 
   Twitter.update("Welcome to Paperless Post, #{session[:name]} @#{session[:twitter]}");
-  obj.public_url
+  obj.public_url.to_s
 end
 
 get '/js/*.*' do
